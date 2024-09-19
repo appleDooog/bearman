14dc9172f7dd186fd0a932981f35e2b356ec3bd0
appleDooog <appledog0113@gmail.com>
2024-09-20 01:49:26 +0800

    admin的menu暫時拿掉

--- web/src/components/admin/index.vue
+++ web/src/components/admin/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="row justify-content-center">
+  <div class="container justify-content-center">
     <div class="col-md-12">
       <div class="row">
-        <div class="col-md-2 d-none d-md-block"><Menu></Menu></div>
+        <!-- <div class="col-md-2 d-none d-md-block"><Menu></Menu></div> -->
         <div class="col-md mt-3">
           <RouterView></RouterView>
         </div>
